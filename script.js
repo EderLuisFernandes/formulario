9b59973a2b4870cfb828e5b662e9765fb435af30
Eder Luis <95106788+EderLuisFernandes@users.noreply.github.com>
2025-05-22 11:35:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('telefone').addEventListener('input', function(event) {
+  let value = event.target.value.replace(/\D/g, '');
+  
+ 
+  event.target.value = value
+    .replace(/^(\d{2})(\d{0,5})(\d{0,4})/, '($1) $2-$3')
+    .slice(0, 15); 
+});
+
+
+
+
